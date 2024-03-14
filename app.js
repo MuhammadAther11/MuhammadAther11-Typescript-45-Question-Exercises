5f3dfbe681c19dd3289bbad15054e2a25427c577
MuhammadAther11 <162538663+MuhammadAther11@users.noreply.github.com>
2024-03-15 00:20:31 +0500

    Ex:3

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
-var personName = "ERIC";
-console.log("hello ".concat(personName, " would you like to learn some python today?"));
+// lower case
+var personName = "Ather";
+console.log("lowercase:", personName.toLowerCase());
+// upper case
+console.log("uppercase:", personName.toUpperCase());
+// Title case
+console.log("titlecase:", personName.replace(/\bw/g, function (c) { return c.toUpperCase(); }));
